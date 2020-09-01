92dff37b5b5e5cdff92066eed478c235edbe1ea7
roman9891 <roman9891@gmail.com>
2020-09-01 10:14:14 -0400

    add and search done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,43 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactionsArray: []
+  }
+  
+  componentDidMount() {
+    this.fetchTransactions()
+  }
+
+  fetchTransactions = () => {
+    fetch(`http://localhost:6001/transactions`)
+    .then(r => r.json())
+    .then(data => this.setState({transactionsArray: data}))
+  }
+
+  postTransaction = transaction => {
+    fetch(`http://localhost:6001/transactions`, {
+      method: `POST`,
+      headers: {
+        "content-type": `application/json`,
+        accept: `application/json`
+      },
+      body: JSON.stringify(transaction)
+    })
+    .then(r => r.json())
+    .then(newTransaction => this.setState({transactionsArray: [...this.state.transactionsArray, newTransaction]}))
+  }
+
+  appHandler = transaction => {
+    this.postTransaction(transaction)
+  }
+  
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm appHandler={this.appHandler}/>
+        <TransactionsList transactions={this.state.transactionsArray}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  state = {
+    date: ``,
+    description: ``,
+    category: ``,
+    amount: 0
+  }
+  
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value}, () => console.log(this.state))
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    this.props.appHandler(this.state)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.submitHandler} className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.changeHandler} value={this.state.date}/>
+            <input type="text" name="description" placeholder="Description" onChange={this.changeHandler} value={this.state.description}/>
+            <input type="text" name="category" placeholder="Category" onChange={this.changeHandler} value={this.state.category}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.changeHandler}
+              value={this.state.amount}
             />
           </div>
           <button className="ui button" type="submit">
