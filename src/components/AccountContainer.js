1d1b51bb91745c36b36db7d05f7ad7d3a3126a4d
elandau91 <eli.b.landau@gmail.com>
2020-09-01 09:57:25 -0400

    core deliverables 1 & 2 complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,47 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  
+  state = {
+    trans: []
+  }
+  
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+    .then(res => res.json())
+    .then(trans => this.setState({trans: trans}))
+  }
+
+  addTrans = (obj) => {
+    console.log(obj)
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify({
+        date: obj.date,
+        description: obj.description,
+        category: obj.category,
+        amount: parseInt(obj.amount) 
+      })
+    }
+
+    fetch("http://localhost:6001/transactions", options)
+    .then(res => res.json())
+    .then(newObj => this.setState({
+      trans: [...this.state.trans, newObj]
+    }))
+  }
+  
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm addTrans={this.addTrans}/>
+        <TransactionsList trans={this.state.trans}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,51 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    // console.log(this.state)
+    this.props.addTrans(this.state)
+    this.setState({
+      date: "",
+      description: "",
+      category: "",
+      amount: ""
+    })
+  }
+  
+  
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input onChange={this.changeHandler} value={this.state.date} type="date" name="date" />
+            <input onChange={this.changeHandler} value={this.state.description} type="text" name="description" placeholder="Description" />
+            <input onChange={this.changeHandler} value={this.state.category} type="text" name="category" placeholder="Category" />
             <input
+              onChange={this.changeHandler} 
+              value={this.state.amount}
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" onClick={this.submitHandler}>
             Add Transaction
           </button>
         </form>
