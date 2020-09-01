593d35abddcb1d9bcd4d212ec91bbee9a1768076
Luis Reyes <luisreyesxv@yahoo.com>
2020-09-01 10:36:26 -0700

    basic deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -13,30 +13,60 @@ class AccountContainer extends Component {
   }
     api = "http://localhost:6001/transactions/"
 
-      componentDidMount=()=>{
-        this.getTransactions()
-      }
-
-
-      getTransactions=()=>{
-        fetch(this.api)
+    
+    
+    getTransactions=(options={method: "GET"})=>{
+      // i was planning on making this DRYer by reusing this, maybe even passing a callback function for the 3rd then statement, but i'm just gonna finish the coding challenge before i try to make this "prettier"
+      fetch(this.api,options)
         .then(response => response.json())
         .then(transactionObj => this.setState({
           ...this.state,
           originalAPI: transactionObj,
           transactions: transactionObj
-
+          
         })
         )
-
       }
 
+      submitTransaction =(transaction)=>{
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"},
+            body: JSON.stringify(transaction)
+          }
 
+        fetch(this.api,options)
+        .then(response => response.json())
+        .then(transactionObj => {
+           const newTransactions = this.state.originalAPI
+           newTransactions.push(transactionObj)
+          this.setState({
+          ...this.state,
+          originalAPI: newTransactions,
+          transactions: newTransactions
+          
+        })
+      }
+        )
+        
+      }
+      
+      searchingTransactions =(event)=>{
+        const searchedTransactions = this.state.originalAPI.filter((transaction)=> transaction.description.includes(event.target.value))
+        this.setState({...this.state,
+        transactions: searchedTransactions})
+      }
+      
+      componentDidMount=()=>{
+        this.getTransactions()
+      }
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
+        <Search searchingTransactions={this.searchingTransactions}/>
+        <AddTransactionForm submitTransaction={this.submitTransaction}/>
         <TransactionsList transactions={this.state.transactions} />
       </div>
     );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  constructor(){
+    super()
+    this.state={
+      date:"",
+      description:"",
+      category:"",
+      amount:""
+    }
+  }
+
+  changeHandler=(event)=>{
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitHandler =(event)=>{
+    event.preventDefault()
+    this.props.submitTransaction(this.state)
+  }
+
+
+
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.submitHandler}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date"   value={this.state.date} onChange={this.changeHandler}/>
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.changeHandler} />
+            <input type="text" name="category" placeholder="Category"  value={this.state.category} onChange={this.changeHandler} />
             <input
+             value={this.state.amount} onChange={this.changeHandler}
               type="number"
               name="amount"
               placeholder="Amount"
