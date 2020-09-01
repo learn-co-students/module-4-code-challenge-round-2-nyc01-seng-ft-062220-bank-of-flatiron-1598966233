3ababdee6e20629150b09d0a40c1272c523ca91d
kylejb <kjburda@gmail.com>
2020-09-01 09:57:13 -0400

    constructed framework for controlled form to create new transactions

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    // year-mm-dd
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
+  }
+
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.changeHandler} />
+            <input type="text" name="description" placeholder="Description" onChange={this.changeHandler} />
+            <input type="text" name="category" placeholder="Category" onChange={this.changeHandler} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.changeHandler}
             />
           </div>
           <button className="ui button" type="submit">
