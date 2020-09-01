6ea1c900bcf24cde129e7ced8355f08dec9b981a
roman9891 <roman9891@gmail.com>
2020-09-01 10:56:02 -0400

    deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,7 +5,8 @@ import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
   state = {
-    transactionsArray: []
+    transactionsArray: [],
+    filteredTransactions: []
   }
   
   componentDidMount() {
@@ -15,7 +16,10 @@ class AccountContainer extends Component {
   fetchTransactions = () => {
     fetch(`http://localhost:6001/transactions`)
     .then(r => r.json())
-    .then(data => this.setState({transactionsArray: data}))
+    .then(data => this.setState({
+      transactionsArray: data,
+      filteredTransactions: data
+    }))
   }
 
   postTransaction = transaction => {
@@ -28,19 +32,41 @@ class AccountContainer extends Component {
       body: JSON.stringify(transaction)
     })
     .then(r => r.json())
-    .then(newTransaction => this.setState({transactionsArray: [...this.state.transactionsArray, newTransaction]}))
+    .then(newTransaction => this.setState({
+      transactionsArray: [...this.state.transactionsArray, newTransaction],
+      filteredTransactions: [...this.state.transactionsArray, newTransaction]
+    }, () => this.searchHandler(this.state.search)))
   }
 
   appHandler = transaction => {
     this.postTransaction(transaction)
   }
+
+  searchHandler = search => {
+    const filteredTransactions = this.state.transactionsArray
+    .filter(transaction => transaction.description.toLowerCase().includes(search.toLowerCase()))
+
+    this.setState({
+      filteredTransactions: filteredTransactions,
+      search: search
+    })
+    //assigned filteredarray to its own state so that the search wouldnt overwrite the original list
+    //if it overwrites the list then you can't backspace in search to see more results
+    //this way allows backspacing but doesn't let posts show up without refreshing or searching
+    //I could have posts also add to filtered array instead of just original array but then it would
+    //erronuously display posts in the middle of searches at least until more characters 
+    //were typed and the search refiltered
+    //OK nevermind it works
+    //I saved the search to state, added posts to the filteredarray, 
+    //and refiltered after each post using the state search
+  }
   
   render() {
     return (
       <div>
-        <Search />
+        <Search searchHandler={this.searchHandler}/>
         <AddTransactionForm appHandler={this.appHandler}/>
-        <TransactionsList transactions={this.state.transactionsArray}/>
+        <TransactionsList transactions={this.state.filteredTransactions}/>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-const Search = () => {
-  return (
-    <div className="ui large fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  );
+class Search extends React.Component {
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value}, () => console.log(this.state))
+    this.props.searchHandler(e.target.value)
+  }
+  
+  render() {
+    return (
+      <div className="ui large fluid icon input">
+        <input
+          type="text"
+          name="search"
+          placeholder={"Search your Recent Transactions"}
+          onChange={(e) => {
+            console.log("Searching...");
+            this.changeHandler(e)
+          }}
+        />
+        <i className="circular search link icon"></i>
+      </div>
+    );
+  }
 };
 
 export default Search;
