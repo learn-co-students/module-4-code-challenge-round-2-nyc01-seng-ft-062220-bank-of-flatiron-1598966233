0eb01ef68d000bed44500c5d4474ba75f9e99462
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-01 09:36:11 -0400

    completed first deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,35 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+var URL = 'http://localhost:6001/transactions/'
+
 class AccountContainer extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      isLoaded : false
+    }
+  }
+
+  componentDidMount () {
+    fetch(URL)
+      .then(response => response.json())
+      .then(transactions => {
+        console.log(transactions)
+        this.setState({
+          transactions : transactions,
+          isLoaded : true
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        {this.state.isLoaded === true ? <TransactionsList transactions={this.state.transactions}/> : "Loading!" }
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,42 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {/* render Transactions here */}
-      </tbody>
-    </table>
-  );
-};
+class TransactionsList extends React.Component {
+
+  mapTransactions = () => {
+    return (
+      this.props.transactions.map(transaction => {
+        return (
+          <Transaction key={transaction.id} transaction={transaction}/>
+        )
+      })
+    )
+  }
+
+  render () {
+    return (
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">Date</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Description</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Category</h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">Amount</h3>
+            </th>
+          </tr>
+          {this.mapTransactions()}
+        </tbody>
+      </table>
+    );
+  }
+}
+
 
 export default TransactionsList;
