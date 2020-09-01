586ec38c7c9d7c72ed21fc3189342aabd8ea8136
kylejb <kjburda@gmail.com>
2020-09-01 09:48:11 -0400

    render list of transactions to table

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,23 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    myTransactions: []
+  }
+
+  async componentDidMount(){
+    let response = await fetch("http://localhost:6001/transactions")
+    let data = await response.json()
+    this.setState({myTransactions: data})
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.myTransactions}/>
       </div>
     );
   }
