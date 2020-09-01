280d8bcd61de13f951da1822e2516a342f98c002
scubaguba345 <brandon.gubitosa@gmail.com>
2020-09-01 10:51:37 -0400

    last push

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Search = (props) => {
+  //console.log(props)
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        value={props.value}
+        //value={props.value}
         onChange={event => props.handleChange(event.target.value)}
         />
       <i className="circular search link icon"></i>
